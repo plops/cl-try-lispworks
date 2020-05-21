5fbb371c7496fee7ad1332faebdcd0cc92195f0d
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-05-21 15:13:24 +0200

    documentation link

--- st.lisp
+++ st.lisp
@@ -2,7 +2,8 @@
 ;; i want to see how well capi works
 
 ;; copy and paste the following into lispworks personal console:
-
+;; /usr/local/lib64/LispWorksPersonal/lispworks-personal-7-1-2-amd64-linux
+;; /usr/local/lib64/LispWorks*/lispw*-amd64-linux
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
@@ -11,12 +12,15 @@
 
 
 ;; in emacs run M-x slime-connect
+;; say yes when asked if to close old connection
 
 (defpackage "g"
   (:add-use-defaults t)
   (:use "CAPI"))
 
 (in-package "g")
+
+;; http://www.lispworks.com/documentation/lw71/CAPI-M/html/capi-m.htm
 
 (setf interface (make-instance 'interface
 			       :visible-min-width 200
